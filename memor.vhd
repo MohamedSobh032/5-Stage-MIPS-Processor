6c217b18272721e89295ccfee84b24ea622ac88b
MohamedSobh032 <mohamed.sobh032@gmail.com>
2024-04-14 12:42:17 +0200

    memor v0.1

--- memor.vhd
+++ memor.vhd
@@ -3,17 +3,17 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY memor IS
-    PORT (
-	    CLK : IN STD_LOGIC;
-	    RST : IN STD_LOGIC;
+	PORT (
+		CLK : IN STD_LOGIC;
+	    	RST : IN STD_LOGIC;
 		Addr  : IN STD_LOGIC_VECTOR(11 DOWNTO 0);
-    	-- read
-    	MemRead   : IN STD_LOGIC;
-    	ReadData  : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-    	-- write
-    	MemWrite  : IN STD_LOGIC;
-    	WriteData : IN STD_LOGIC_VECTOR(15 DOWNTO 0)
-    );
+    		-- read
+    		MemRead   : IN STD_LOGIC;
+    		ReadData  : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+    		-- write
+    		MemWrite  : IN STD_LOGIC;
+    		WriteData : IN STD_LOGIC_VECTOR(15 DOWNTO 0)
+    	);
 END memor;
 
 
